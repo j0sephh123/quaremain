c6e531dcf7518a472854bb6c5e77bff73c8bf1b4
Momozor <skelic3@gmail.com>
2020-04-14 13:58:16 +0800

    Add first unit test

--- tests/main.lisp
+++ tests/main.lisp
@@ -7,7 +7,7 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :quaremain)' in your Lisp.
 
 (deftest unit-tests
-  (testing "(sum-model '((:cost-per-package 2.2 :amount 3))) should equal to ((:cost-per-package 6.6 :amount 3)"
+  (testing "(sum-model '((:cost-per-package 2.2 :amount 3))) should equal to ((:cost-per-package 6.6000004 :amount 3)"
            (let* ((raw-result
                    (quaremain.web::sum-model '((:cost-per-package 2.2 :amount 3))))
                   (total-cost (getf (car raw-result) :cost-per-package)))
