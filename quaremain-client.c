c2ecd5eeaf61f40520d182b686ba9fbd7602dd9f
Momozor <skelic3@gmail.com>
2020-04-14 19:10:11 +0800

    Fixed the damn indentation

--- quaremain-client.c
+++ quaremain-client.c
@@ -35,51 +35,51 @@ static gboolean closeWebViewCb(WebKitWebView* webView, GtkWidget* window);
 
 int main(int argc, char* argv[])
 {
-    // Initialize GTK+
-    gtk_init(&argc, &argv);
+  // Initialize GTK+
+  gtk_init(&argc, &argv);
 
-    // Create an 800x600 window that will contain the browser instance
-    GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 600);
+  // Create an 800x600 window that will contain the browser instance
+  GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 600);
 
-    WebKitWebContext *context = webkit_web_context_new();
-    webkit_web_context_set_cache_model(context, WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER);
+  WebKitWebContext *context = webkit_web_context_new();
+  webkit_web_context_set_cache_model(context, WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER);
     
-    // Create a browser instance
-    WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(context));
+  // Create a browser instance
+  WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(context));
 
-    // Put the browser area into the main window
-                                             gtk_container_add(GTK_CONTAINER(main_window), GTK_WIDGET(webView));
+  // Put the browser area into the main window
+  gtk_container_add(GTK_CONTAINER(main_window), GTK_WIDGET(webView));
 
-                                             // Set up callbacks so that if either the main window or the browser instance is
-                                             // closed, the program will exit
-                                             g_signal_connect(main_window, "destroy", G_CALLBACK(destroyWindowCb), NULL);
-                                             g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), main_window);
+  // Set up callbacks so that if either the main window or the browser instance is
+  // closed, the program will exit
+  g_signal_connect(main_window, "destroy", G_CALLBACK(destroyWindowCb), NULL);
+  g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), main_window);
 
-                                             // Load a web page into the browser instance
-                                             webkit_web_view_load_uri(webView, "http://127.0.0.1:5000");
+  // Load a web page into the browser instance
+  webkit_web_view_load_uri(webView, "http://127.0.0.1:5000");
 
-                                             // Make sure that when the browser area becomes visible, it will get mouse
-                                             // and keyboard events
-                                             gtk_widget_grab_focus(GTK_WIDGET(webView));
+  // Make sure that when the browser area becomes visible, it will get mouse
+  // and keyboard events
+  gtk_widget_grab_focus(GTK_WIDGET(webView));
 
-                                             // Make sure the main window and all its contents are visible
-                                             gtk_widget_show_all(main_window);
+  // Make sure the main window and all its contents are visible
+  gtk_widget_show_all(main_window);
 
-                                             // Run the main GTK+ event loop
-                                             gtk_main();
+  // Run the main GTK+ event loop
+  gtk_main();
 
-                                             return 0;
-                                             }
+  return 0;
+}
 
 
 static void destroyWindowCb(GtkWidget* widget, GtkWidget* window)
 {
-    gtk_main_quit();
+  gtk_main_quit();
 }
 
 static gboolean closeWebViewCb(WebKitWebView* webView, GtkWidget* window)
 {
-    gtk_widget_destroy(window);
-    return TRUE;
+  gtk_widget_destroy(window);
+  return TRUE;
 }
