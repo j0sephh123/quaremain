3b063482025703ab982ed4bcbe37c6cce2fcb612
Momozor <skelic3@gmail.com>
2020-04-20 16:02:59 +0800

    Bump to version 0.4.2

--- version-bumper.sh
+++ version-bumper.sh
@@ -7,5 +7,6 @@ README_REP="s/^[0-9]\.[0-9]\.[0-9]$/${CURRENT_VERSION}/"
 ASDF_REP="s/[0-9]\.[0-9]\.[0.9]/${CURRENT_VERSION}/"
 ASDF_ROOT=./quaremain.asd
 
+sed -i $README_REP $README_DIST
 sed -i $README_REP $README_ROOT
 sed -i $ASDF_REP $ASDF_ROOT
