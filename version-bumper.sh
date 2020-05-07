01bdde3aed2b063ec1023613ce1ec040ed8d7122
Momozor <skelic3@gmail.com>
2020-05-07 17:14:07 +0800

    Update about

--- version-bumper.sh
+++ version-bumper.sh
@@ -9,10 +9,7 @@ README_ROOT=./README.md
 README_REP="s/^[0-9]\.[0-9]\.[0-9]$/${CURRENT_VERSION}/"
 ASDF_REP="s/[0-9]\.[0-9]\.[0-9]\"$/${CURRENT_VERSION}\"/"
 ASDF_ROOT=./quaremain.asd
-ABOUT_REP="s/[0-9]\.[0-9]\.[0-9]/${CURRENT_VERSION}/"
-ABOUT_ROOT=./templates/about.html
 
 sed -i $README_REP $README_DIST
 sed -i $README_REP $README_ROOT
 sed -i $ASDF_REP $ASDF_ROOT
-sed -i $ABOUT_REP $ABOUT_ROOT
