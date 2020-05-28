dbf80d53e0cf3781f169cae6a4e2cd1b793777b3
Momozor <skelic3@gmail.com>
2020-05-28 20:49:14 +0800

    Simplify

--- quaremain-client.c
+++ quaremain-client.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2020, Momozor
  * Copyright (C) 2006, 2007 Apple Inc.
  * Copyright (C) 2007 Alp Toker <alp@atoker.com>
  * Copyright (C) 2011 Lukasz Slachciak
@@ -30,18 +31,34 @@
 #include <webkit2/webkit2.h>
 
 
-static void destroyWindowCb();
-static gboolean closeWebViewCb(WebKitWebView* webView, GtkWidget* window);
+static void
+destroyWindowCallback();
+
+static gboolean
+closeWebViewCallback(WebKitWebView* webView, GtkWidget* window);
+
+
+static void
+destroyWindowCallback() {
+  gtk_main_quit();
+}
+
+static gboolean
+closeWebViewCallback(
+                     __attribute__((unused))WebKitWebView* webView,
+                     GtkWidget* window
+                     ) {
+  gtk_widget_destroy(window);
+  return TRUE;
+}
+
 
 #define DEFAULT_HOST "http://127.0.0.1"
 #define DEFAULT_PORT "5000"
 
-int main(int argc, char* argv[])
-{
-  // Initialize GTK+
+int main(int argc, char* argv[]) {
   gtk_init(&argc, &argv);
 
-  // Create an 800x600 window that will contain the browser instance
   GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(main_window), "Quaremain");
   gtk_window_set_default_size(GTK_WINDOW(main_window), 1280, 720);
@@ -49,52 +66,32 @@ int main(int argc, char* argv[])
   WebKitWebContext *context = webkit_web_context_new();
   webkit_web_context_set_cache_model(context, WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER);
     
-  // Create a browser instance
   WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(context));
 
-  // Put the browser area into the main window
   gtk_container_add(GTK_CONTAINER(main_window), GTK_WIDGET(webView));
 
-  // Set up callbacks so that if either the main window or the browser instance is
-  // closed, the program will exit
-  g_signal_connect(main_window, "destroy", G_CALLBACK(destroyWindowCb), NULL);
-  g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), main_window);
+  g_signal_connect(main_window, "destroy", G_CALLBACK(destroyWindowCallback), NULL);
+  g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCallback), main_window);
 
   char address_buffer[80];
   char* port = getenv("QUAREMAIN_PORT");
 
+  sprintf(address_buffer, "%s:%s", DEFAULT_HOST, DEFAULT_PORT);
   if (port != NULL) {
     sprintf(address_buffer, "%s:%s", DEFAULT_HOST, port);
-  }
-  else {
-    sprintf(address_buffer, "%s:%s", DEFAULT_HOST, DEFAULT_PORT);
   }
   
   // Load a web page into the browser instance
   webkit_web_view_load_uri(webView, address_buffer);
 
   // Make sure that when the browser area becomes visible, it will get mouse
-    // and keyboard events
+  // and keyboard events
   gtk_widget_grab_focus(GTK_WIDGET(webView));
 
   // Make sure the main window and all its contents are visible
   gtk_widget_show_all(main_window);
 
-  // Run the main GTK+ event loop
   gtk_main();
 
   return 0;
-}
-
-
-static void destroyWindowCb()
-{
-  gtk_main_quit();
-}
-
-static gboolean closeWebViewCb(__attribute__((unused))WebKitWebView* webView,
-                               GtkWidget* window)
-{
-  gtk_widget_destroy(window);
-  return TRUE;
 }
