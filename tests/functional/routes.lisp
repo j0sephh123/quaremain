cba59860a703e80c7fe11eb5b0801952c81d09a7
Momozor <skelic3@gmail.com>
2020-06-17 20:09:01 +0800

    Add first API route test

--- tests/functional/routes.lisp
+++ tests/functional/routes.lisp
@@ -2,17 +2,30 @@
   (:use :cl
         :quaremain
         :rove)
+  (:local-nicknames (#:server #:quaremain))
   (:local-nicknames (#:routes #:quaremain.web))
   (:local-nicknames (#:database #:quaremain.utilities.database)))
 (in-package :quaremain/tests/functional/routes)
 
 (setup
-  (database::migrate-tables)
+  (server:start :port 5000 :debug t)
   (database::migrate-seeds))
 
 (teardown
-  (database::drop-tables))
+  (database::drop-tables)
+  (server:stop))
 
-(deftest tests
+(defparameter +root-host+ "http://localhost:5000")
+
+(defun get-content (root-host route)
+  (dex:get (format nil "~a~a" root-host route)))
+
+(defun get-ok (route)
+  (eql
+   (nth-value 1 (get-content +root-host+ route))
+   200))
+
+(deftest index-or-experimental-route
   (testing ""
-    (ok (= 1 1))))
+    (ok
+     (get-ok "/"))))
